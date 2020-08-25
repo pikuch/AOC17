73c0f8331295d5ae090a00900ddf67218fcf9366
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-25 10:33:43 +0200

    day 1 done

--- Day01.py
+++ Day01.py
@@ -1,5 +1,12 @@
 # AOC17 day 01
 
+def calculate_sum(s, ahead):
+    digit_sum = 0
+    for i in range(len(s)):
+        if s[i] == s[(i + ahead) % len(s)]:
+            digit_sum += int(s[i])
+    return digit_sum
+
 
 def load_data(f_name):
     with open(f_name, "r") as f:
@@ -9,3 +16,5 @@ def load_data(f_name):
 
 def run():
     data = load_data("Day01.txt")
+    print(f"The sum of digits repeating one ahead is {calculate_sum(data, 1)}")
+    print(f"The sum of digits repeating half way around ahead is {calculate_sum(data, len(data)//2)}")
