3dbb2e08fd1e35bf7f24cba00400e04d93bbd594
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-25 12:13:50 +0200

    day 4 done

--- Day04.py
+++ Day04.py
@@ -1,4 +1,21 @@
 # AOC17 day 04
+from itertools import combinations, permutations
+
+
+def is_valid(s):
+    words = s.split()
+    for comb in combinations(words, 2):
+        if comb[0] == comb[1]:
+            return False
+    return True
+
+
+def is_valid_strict(s):
+    words = s.split()
+    for comb in combinations(words, 2):
+        if len(comb[0]) == len(comb[1]) and tuple(comb[0]) in permutations(comb[1]):
+            return False
+    return True
 
 
 def load_data(f_name):
@@ -9,4 +26,6 @@ def load_data(f_name):
 
 def run():
     data = load_data("Day04.txt")
-
+    phrases = data.split("\n")
+    print(f"There are {sum(map(is_valid, phrases))} valid passphrases")
+    print(f"There are {sum(map(is_valid_strict, phrases))} valid passphrases when using strict rules")
