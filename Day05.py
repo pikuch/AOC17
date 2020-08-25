1747401b715ab5f63bc65ce36aa54e56eb217d4e
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-25 12:44:21 +0200

    day 5 done

--- Day05.py
+++ Day05.py
@@ -1,6 +1,28 @@
 # AOC17 day 05
 
 
+def escape_program(prog):
+    steps = 0
+    index = 0
+    while 0 <= index < len(prog):
+        new_index = index + prog[index]
+        prog[index] += 1
+        index = new_index
+        steps += 1
+    return steps
+
+
+def escape_program_2(prog):
+    steps = 0
+    index = 0
+    while 0 <= index < len(prog):
+        new_index = index + prog[index]
+        prog[index] += 1 if prog[index] < 3 else -1
+        index = new_index
+        steps += 1
+    return steps
+
+
 def load_data(f_name):
     with open(f_name, "r") as f:
         data_read = f.read()
@@ -9,4 +31,7 @@ def load_data(f_name):
 
 def run():
     data = load_data("Day05.txt")
-
+    prog = list(map(int, data.split("\n")))
+    prog2 = prog[:]  # a copy before modification is needed
+    print(f"It takes {escape_program(prog)} steps to jump out of the program")
+    print(f"It takes {escape_program_2(prog2)} steps to jump out of the program with rule 2")
