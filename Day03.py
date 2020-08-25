8a7e14c7d79ae2307ba41561ebb186c624dc60ab
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-25 11:30:23 +0200

    part 1 done

--- Day03.py
+++ Day03.py
@@ -1,6 +1,26 @@
 # AOC17 day 03
 
 
+def calculate_distance(cell_number):
+    coords = [0, 0]
+    step = -1
+    current = 1
+    axis = 0
+    sign = 0
+    while current < cell_number:
+        step += 1
+        axis = step % 2
+        sign = (((step - 1) // 2) % 2) * 2 - 1
+        dist = step // 2 + 1
+        current += dist
+        coords[axis] += sign * dist
+    # go back a few cells if needed
+    if current > cell_number:
+        coords[axis] -= sign * (current - cell_number)
+    # return manhattan distance
+    return abs(coords[0]) + abs(coords[1])
+
+
 def load_data(f_name):
     with open(f_name, "r") as f:
         data_read = f.read()
@@ -9,4 +29,5 @@ def load_data(f_name):
 
 def run():
     data = load_data("Day03.txt")
-
+    cell = int(data)
+    print(f"Cell {cell} is {calculate_distance(cell)} cells away from center")
