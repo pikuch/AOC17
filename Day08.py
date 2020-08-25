b5d28edf91a5b38564bbd624c5f358b4c5487800
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-25 18:56:45 +0200

    early decoding

--- Day08.py
+++ Day08.py
@@ -1,4 +1,12 @@
 # AOC17 day 08
+from collections import defaultdict
+
+
+def decode(prog):
+    regs = defaultdict(lambda: 0)  # for new registers
+    for words in map(lambda x: x.split(), prog):
+
+
 
 
 def load_data(f_name):
@@ -9,4 +17,6 @@ def load_data(f_name):
 
 def run():
     data = load_data("Day08.txt")
-
+    program = data.split("\n")
+    registers = decode(program)
+    print(f"The largest value in any register is {max(registers.values())}")
