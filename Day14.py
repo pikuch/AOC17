6bb4180c4a0069bfbb77a9dcdb5429e06d7811ce
pikuch <55052790+pikuch@users.noreply.github.com>
2020-08-26 22:04:49 +0200

    day 14 done

--- Day14.py
+++ Day14.py
@@ -20,6 +20,29 @@ def show_grid(grid):
         print("")
 
 
+def remove_region(squares):
+    to_check = [squares.pop()]
+    while len(to_check):
+        row, col = to_check.pop(0)
+        for pair in [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]:
+            if pair in squares:
+                to_check.append(pair)
+                squares.remove(pair)
+
+
+def count_regions(grid):
+    squares = set()
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if grid[row][col]:
+                squares.add((row, col))
+    regions = 0
+    while len(squares):
+        remove_region(squares)
+        regions += 1
+    return regions
+
+
 def load_data(f_name):
     with open(f_name, "r") as f:
         data_read = f.read()
@@ -33,3 +56,4 @@ def run():
     grid = get_grid(data)
     used = sum(map(lambda x: sum(x), grid))
     print(f"There are {used} squares used")
+    print(f"There are {count_regions(grid)} regions present")
